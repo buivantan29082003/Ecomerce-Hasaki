2b8cb63fe263e0761e056104d3a2271edb44b366
buivantan29082003 <buivantan29082003@gmail.com>
2025-10-20 09:45:09 +0700

    e

--- CloneShopee/src/main/java/com/CloneShopee/mapper/sale/productMapper/ProductInfoMapper.java
+++ CloneShopee/src/main/java/com/CloneShopee/mapper/sale/productMapper/ProductInfoMapper.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Component;
 import com.CloneShopee.DTO.Sale.product.ProductInfo;
 import com.CloneShopee.models.Product;
 
-@Mapper(componentModel = "spring") 
+@Mapper(componentModel = "spring")
 public interface ProductInfoMapper {
-	@Mapping(source = "brand.brandName", target = "brandName")
-	List<ProductInfo> productToProductDTO(List<Product> products); 
+	// @Mapping(source = "brand.brandName", target = "brandName")
+	// List<ProductInfo> productToProductDTO(List<Product> products);
 }
